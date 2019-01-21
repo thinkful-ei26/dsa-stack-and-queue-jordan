e02855c93f5ac80df1c051fc8d5f674a56bb9957
Jordan <jordanhaddadi@gmail.com>
2019-01-21 16:07:25 -0500

    reworked palindrome - going over and redoing fridays drills for practice

--- dsa-stack-queue.js
+++ dsa-stack-queue.js
@@ -11,13 +11,14 @@ class _Node {
     }
 }
 
-//create a stack called starTrek
+//create the stack class 
 class Stack {
     constructor(){
         this.top = null;
+        this.count = 0;
     }
 
-    //add data
+    //add data, set it to the top of the stack
     push(data){
         if (this.top === null){
             this.top = new _Node(data, null);
@@ -25,13 +26,24 @@ class Stack {
         }
         const node = new _Node(data, this.top);
         this.top = node;
+        this.count++;
     }
 
     //remove data
     pop(){
-        const node = this.top;
+        let node = this.top;
         this.top = node.next;
+        this.count--;
         return node.data;
+    }
+
+    //get the count/number of items
+    length(){
+        return this.count +1;
+    }
+
+    peek(){
+        return this.top.data;
     }
 }
 
@@ -47,28 +59,40 @@ function main(){
     starTrek.pop('Scotty');
     starTrek.pop('McCoy');
     starTrek.push('Scotty');
-    console.log(starTrek)
-    return main;
+    console.log(starTrek.peek());
+    console.log(starTrek.length());
 }
+// main()
 
-main()
+// if original string = the reverse one, it's a palindrome
+// push each character of the string onto a stack
+// when we pop off of the stack, concat and that will be reversed
 
 function is_palindrome(s){
     s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-    sArr = s.split("");
-    sRev = sArr.reverse();
-    sRevJoin = sRev.join("");
+    let stack = new Stack();
+    let rev = ""
+//create a stack by pushing each letter from the string
+    for (let i = 0; i < s.length; i++){
+        stack.push(s[i]);
+    }
 
-    if(s === sRevJoin){
+//pop off each letter starting from the END and add it to rev
+    for (let i = 0; i <s.length; i++){
+        rev += stack.pop(s[i]);
+    }
+//check if the original string equals the newly reversed string
+    if(s === rev){
         return true;
     }
     else{
         return false;
     }
+
 };
 
 //expected output: true, true, true, false
 console.log(is_palindrome("dad"));
 console.log(is_palindrome("A man, a plan, a canal: Panama"));
 console.log(is_palindrome("1001"));
-console.log(is_palindrome("Tauhida"));
+console.log(is_palindrome("Jordan"));
